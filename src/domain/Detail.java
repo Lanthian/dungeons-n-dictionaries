37a7143f6ba09eeba34b5242bdffd98974d55ed7
Lanthian <lanthian@student.unimelb.edu.au>
2025-11-15 16:47:45 +1100

    feat: implement abstract Described class and Detail class

---
+++ src/domain/Detail.java
@@ -0,0 +1,43 @@
+// src/domain/Detail.java
+package domain;
+
+/**
+ * Additional textual information that can be appended to any fixed entity.
+ * Details are sorted by descending {@code order}, then default 
+ * {@link Described} sorting.
+ */
+public class Detail extends Described {
+
+    // --- Constants ---
+    private static final int DEFAULT_ORDER = 0;
+    
+    // --- Attributes ---
+    private int order;
+
+    // Constructor 
+    public Detail(String name, String description, int order) {
+        super(name, description);
+        this.order = order;
+    }
+
+    // Overloaded Constructor (optional order)
+    public Detail(String title, String description) {
+        this(title, description, DEFAULT_ORDER);
+    }
+
+    /* ======================================================================
+     * -------------------------- Comparison Logic -------------------------- 
+     * ====================================================================== */
+
+    @Override
+    protected int compareBefore(Described o) {
+        // Cast `Described` object to class `Detail`
+        if (o.getClass() != this.getClass()) {
+            throw new ClassCastException("Class mismatch - Described objects cannot be differentiated on order");
+        }
+        Detail cast = (Detail) o;
+
+        // Compare ordering (descending)
+        return ((Integer) cast.order).compareTo(this.order);
+    }
+}
