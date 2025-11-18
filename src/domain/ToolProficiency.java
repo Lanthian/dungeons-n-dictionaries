5eb3ae25ef3604442c57417099c4da01ec0def6d
Lanthian <lanthian@student.unimelb.edu.au>
2025-11-18 23:42:42 +1100

    feat: define select proficiencies
    1. Skill, Tool and Armour Proficiency-s defined
    2. Relevant ToolType and ArmourType enums defined

---
+++ src/domain/ToolProficiency.java
@@ -0,0 +1,29 @@
+// src/domain/ToolProficiency.java
+package domain;
+
+/**
+ * A {@link Proficiency} providing a {@link Character} with expertise regarding
+ * a particular set of tools. 
+ * 
+ * <p> ToolProficiency {@code name} is the relevant tool's name, 
+ * {@code description} describes the tool.
+ */
+public class ToolProficiency extends Described implements Proficiency {
+
+    // --- Attributes ---
+    private ToolType type;
+
+    // Constructor
+    public ToolProficiency(String name, String description, ToolType toolType) {
+        super(name, description);
+        this.type = toolType;
+    }
+    
+    // Overloaded Constructor (defaults Miscellaneous)
+    public ToolProficiency(String name, String description) {
+        this(name, description, ToolType.MISCELLANEOUS);
+    }
+
+    // --- Getter ---
+    public ToolType getType() { return this.type; }
+}
