df2f81737a840c31ea8cd9338df9f617d65b893f
Lanthian <lanthian@student.unimelb.edu.au>
2025-12-30 21:39:21 +1100

    feat: proficiency api defined in frontend

---
+++ src/frontend/src/features/proficiency/types/proficiency.ts
@@ -0,0 +1,36 @@
+// features/proficiency/types/proficiency.ts
+/**
+ * Base interface extended by all concrete Proficiency types. `id` is optional
+ * to allow the posting of new proficiencies. `proficiencyType` differentiates
+ * between types, and is a key field for the backend to decipher which kind of
+ * proficiency it is handling.
+ */
+interface BaseProficiency {
+  id?: string;
+  proficiencyType: string;
+};
+
+/**
+ * Union of all known proficiencies modelled in the frontend.
+ */
+export type Proficiency =
+  | ArmourProficiency
+  | SkillProficiency
+  | ToolProficiency;
+
+export interface ArmourProficiency extends BaseProficiency {
+  proficiencyType: "ARMOUR";
+  type: string;
+};
+
+export interface SkillProficiency extends BaseProficiency {
+  proficiencyType: "SKILL";
+  skill: string;
+};
+
+export interface ToolProficiency extends BaseProficiency {
+  proficiencyType: "TOOL";
+  name: string;
+  description: string;
+  type: string;
+};
